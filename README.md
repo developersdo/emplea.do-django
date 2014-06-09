b2948ddaa927816383d93d4582c1a9d3c6f14be9
Jearel Alcantara <jeasoft@gmail.com>
2014-06-08 21:55:14 -0400

    Segundo commit, solo unos cambios en el Readme.MD e incorporando la aplicacion

---
+++ README.md
@@ -0,0 +1,8 @@
+# Emplea.do #
+
+Simple, esta es una versión de [emplea.do][1], pero hecha en [Django][2]
+
+Si quieren cooperar, no hay problemas.
+
+  [1]: http://www.emplea.do "Emplea.DO"
+  [2]: https://www.djangoproject.com/ "Django"
---
+++ emplea_do/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for emplea_do project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emplea_do.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
