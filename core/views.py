f4df9d007a99cb134188c103e106e716017e72e2
Jearel Alcantara <jeasoft@gmail.com>
2014-06-09 18:39:48 -0400

    creación de templates para listar los trabajos disponibles (job_list.html) y los detalles de los mismo (job_detail.html), ademas de un formulario, creación de sus vistas y añadiendolos a los urls

--- core/views.py
+++ core/views.py
@@ -1,17 +1,20 @@
 # Vistas
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views import generic
 from models import Job, Category
 
-class JobList(ListView):
+class JobList(generic.ListView):
     context_object_name = "job_list"
     queryset = Job.objects.order_by("-pub_date")
     template_name = "jobs/job_list.html"
 
-class JobDetail(DetailView):
+class JobDetail(generic.DetailView):
     model = Job
     context_object_name = "job"
     template_name = "jobs/job_details.html"
 
-    
-
+class JobCreate(generic.edit.CreateView):
+	model = Job
+	fields = ["name", "category", "place", "description", "application", 
+		"company_name", "url", "email", "logo"]
+	template_name = "jobs/job_form.html"
--- emplea_do/urls.py
+++ emplea_do/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from core.views import JobList, JobDetail
+from core.views import JobList, JobDetail, JobCreate
 
 admin.autodiscover()
 
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'emplea_do.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', JobList.as_view()),
+    url(r'^job/new/$', JobCreate.as_view()),
     url(r'^job/(?P<pk>\d+)/$', JobDetail.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
